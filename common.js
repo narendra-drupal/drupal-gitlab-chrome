03a21f6bd49910380fff9ccf9d8d5a2a2a1a3a85
Ted Bowman <ted+git@tedbow.com>
2022-11-17 20:37:03 -0500

    prettier

--- common.js
+++ common.js
@@ -1,25 +1,23 @@
 const drupalConfig = {
   statusField: {
-    "ACTIVE": "1",
-    "FIXED": "2",
-    "CLOSED_DUPLICATE": "3",
-    "POSTPONED": "4",
-    "CLOSED_WONT_FIX": "5",
-    "CLOSED_WORKS_AS_DESIGNED": "6",
-    "CLOSED_FIXED": "7",
-    "NEEDS_REVIEW": "8",
-    "NEEDS_WORK": "13",
-    "RTBC": "14",
-    "PATCH_TO_BE_PORTED": "15",
-    "POSTPONED_MAINTAINER_NEEDS_MO_INFO": "16",
-    "CLOSED_OUTDATED": "17",
-    "CLOSED_CANNOT_REPRODUCE": "18"
-  }
-
-}
+    ACTIVE: "1",
+    FIXED: "2",
+    CLOSED_DUPLICATE: "3",
+    POSTPONED: "4",
+    CLOSED_WONT_FIX: "5",
+    CLOSED_WORKS_AS_DESIGNED: "6",
+    CLOSED_FIXED: "7",
+    NEEDS_REVIEW: "8",
+    NEEDS_WORK: "13",
+    RTBC: "14",
+    PATCH_TO_BE_PORTED: "15",
+    POSTPONED_MAINTAINER_NEEDS_MO_INFO: "16",
+    CLOSED_OUTDATED: "17",
+    CLOSED_CANNOT_REPRODUCE: "18",
+  },
+};
 const utils = {
-  "getIssueIdFromUrl":
-   function (url) {
+  getIssueIdFromUrl: function (url) {
     let parts = url.split("/");
     let lastPart = parts[parts.length - 1];
     parts = lastPart.split("#");
@@ -27,8 +25,8 @@ const utils = {
   },
   getStatusForId: (id) => {
     return Object.keys(drupalConfig.statusField).find(
-        key => drupalConfig.statusField[key] === id
+      (key) => drupalConfig.statusField[key] === id
     );
-  }
-}
+  },
+};
 export { utils };
