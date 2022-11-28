1d7f0923391bd1a60ebb6a55141acfd5c4bd79a3
Ted Bowman <ted+git@tedbow.com>
2022-11-28 17:11:10 -0500

    remove url check because this is now in manifest.json

--- drupal-content-queue.js
+++ drupal-content-queue.js
@@ -5,18 +5,11 @@
   const { userCount } = await import(src);
   src = chrome.runtime.getURL("dynamic-filter-script.js");
   const { titleFilter } = await import(src);
-
-
-  var url = document.URL;
-  // @todo We could move the pattern matching to the manifest file unless we plan to make this dynamic, for instance
-  //   if we only want to match for particular projects.
-  const issueQueueUrlPattern = /https:\/\/www\.drupal\.org\/project\/issues\/search\/.*/g;
-  if (url.match(issueQueueUrlPattern)) {
-    const customToolbar = document.createElement('div');
-    customToolbar.id = "custom-toolbar";
-    const issueTable = document.querySelector("table.project-issue");
-    customToolbar.appendChild(userCount.createElement());
-    customToolbar.appendChild(titleFilter.createElement());
-    issueTable.parentNode.insertBefore(customToolbar, issueTable);
-  }
+  
+  const customToolbar = document.createElement('div');
+  customToolbar.id = "custom-toolbar";
+  const issueTable = document.querySelector("table.project-issue");
+  customToolbar.appendChild(userCount.createElement());
+  customToolbar.appendChild(titleFilter.createElement());
+  issueTable.parentNode.insertBefore(customToolbar, issueTable);
 })();
