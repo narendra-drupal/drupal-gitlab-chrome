9aa63f814562e53ad8d80514834331828b4c61ae
Ted Bowman <ted+git@tedbow.com>
2022-11-28 13:13:00 -0500

    create drupal-user-count.js to split out functionality

--- drupal-content-script.js
+++ drupal-content-script.js
@@ -1,85 +1,19 @@
 (async () => {
-  const src = chrome.runtime.getURL("common.js");
+  let src = chrome.runtime.getURL("common.js");
   const { utils } = await import(src);
+  src = chrome.runtime.getURL("drupal-user-count.js");
+  const { userCount } = await import(src);
+
 
   var url = document.URL;
-  const regex = /https:\/\/www\.drupal\.org\/project\/issues\/search\/.*/g;
-  if (url.match(regex)) {
-    // On issue queue search page
-    const assignedInput = document.getElementById("edit-assigned");
-    const assignedText = assignedInput.getAttribute("value").trim();
-    const assignedFields = document.querySelectorAll(
-      "td.views-field-field-issue-assigned"
-    );
-    let names = [];
-    if (assignedText) {
-      names = assignedText.split(",").map((name) => name.trim());
-    } else {
-      names = [
-        ...new Set(
-          [...assignedFields].map((assignedField) =>
-            assignedField.innerText.trim()
-          )
-        ),
-      ];
-    }
-
-    let userIssues = [];
-
-    const usersDiv = document.createElement("div");
-    usersDiv.className = "usersCount";
-    names.forEach((name) => {
-      let userIssue = { name: name, count: 0 };
-      assignedFields.forEach((field) => {
-        if (field.textContent.includes(name)) {
-          userIssue.count++;
-        }
-      });
-      const userDiv = document.createElement("div");
-      userDiv.innerText = `${name}: ${userIssue.count}`;
-      if (userIssue.count) {
-        const clicker = document.createElement("a");
-        //clicker.setAttribute('href', '#')
-        clicker.setAttribute("user", name);
-        clicker.onclick = function (event) {
-          const target = event.target;
-          const targetIsFiltered = target.hasAttribute("filtered");
-          document.querySelectorAll(".user-filter").forEach((link) => {
-            link.innerText = "🔎";
-            link.removeAttribute("filtered");
-          });
-          if (targetIsFiltered) {
-            assignedFields.forEach(
-              (assignedField) =>
-                (assignedField.closest("tr").style.display = "table-row")
-            );
-            return;
-          }
-
-          target.innerText = "✅";
-          target.setAttribute("filtered", true);
-          assignedFields.forEach((assignedField) => {
-            const parentRow = assignedField.closest("tr");
-            if (assignedField.innerText.includes(name)) {
-              parentRow.style.display = "table-row";
-            } else {
-              parentRow.style.display = "none";
-            }
-          });
-        };
-        clicker.innerText = "🔎";
-        clicker.className = "user-filter";
-        userDiv.appendChild(clicker);
-      }
-
-      userDiv.className = `user-issue-cnt-${userIssue.count}`;
-      usersDiv.appendChild(userDiv);
-      userIssues.push(userIssue);
-    });
+  // @todo We could move the pattern matching to the manifest file unless we plan to make this dynamic, for instance
+  //   if we only want to match for particular projects.
+  const issueQueueUrlPattern = /https:\/\/www\.drupal\.org\/project\/issues\/search\/.*/g;
+  if (url.match(issueQueueUrlPattern)) {
+    const customToolbar = document.createElement('div');
+    customToolbar.id = "custom-toolbar";
     const issueTable = document.querySelector("table.project-issue");
-    issueTable.parentNode.insertBefore(usersDiv, issueTable);
-
-    issueTable.insertBefore();
-    //alert('d');
+    customToolbar.appendChild(userCount.createElement())
+    issueTable.parentNode.insertBefore(customToolbar, issueTable);
   }
 })();
