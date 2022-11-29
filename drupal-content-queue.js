41ea2d73499a0657fe312964e6c3ba789959c1c8
Ted Bowman <ted+git@tedbow.com>
2022-11-29 12:34:31 -0500

    working

--- drupal-content-queue.js
+++ drupal-content-queue.js
@@ -1,7 +1,4 @@
 (async () => {
-  if (!matchesProjectSetting()) {
-    return;
-  }
   // @todo Is there a better way to import these files. Wanted to have import functionality that works for both content
   //   and background scripts.
   let src = chrome.runtime.getURL("common.js");
@@ -12,29 +9,24 @@
   src = chrome.runtime.getURL("dynamic-filter-script.js");
   const { titleFilter } = await import(src);
 
-
-  const customToolbar = document.createElement('div');
-  customToolbar.id = "custom-toolbar";
-  const issueTable = document.querySelector("table.project-issue");
-  // Add the individual elements to the toolbar.
-  customToolbar.appendChild(userCount.createElement());
-  customToolbar.appendChild(titleFilter.createElement());
-  issueTable.parentNode.insertBefore(customToolbar, issueTable);
-
-  async function matchesProjectSetting() {
-    let matched = false;
-    await chrome.storage.sync.get({
-      projects: []
-    }, function(items) {
-      items.projects.every(project => {
+  chrome.storage.sync.get(
+    {
+      projects: [],
+    },
+    function (items) {
+      items.projects.every((project) => {
         if (document.URL.includes(`issues/search/${project}`)) {
-          matched = true;
+          const customToolbar = document.createElement("div");
+          customToolbar.id = "custom-toolbar";
+          const issueTable = document.querySelector("table.project-issue");
+          // Add the individual elements to the toolbar.
+          customToolbar.appendChild(userCount.createElement());
+          customToolbar.appendChild(titleFilter.createElement());
+          issueTable.parentNode.insertBefore(customToolbar, issueTable);
           return false;
         }
         return true;
       });
-    });
-    return matched;
-  }
-
+    }
+  );
 })();
