37b39eba5d4f0609d746df10c7b758fcabbe5db5
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2022-12-01 14:56:27 +0530

    Some progress

--- gitlab.js
+++ gitlab.js
@@ -1,3 +1,30 @@
 (async () => {
-  console.log('ddd');
+  function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+
+  waitForElm('.js-discussion-container').then(() => {
+    const allLinks = document.querySelectorAll('.note-text a');
+    for (const anchorElement of allLinks) {
+      console.log(anchorElement);
+    }
+
+  });
+
 })();
