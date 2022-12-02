192539d475b62f2b0346bae449c1e57f0beae2a5
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2022-12-02 10:48:49 +0530

    Status links updated

--- gitlab.js
+++ gitlab.js
@@ -3,12 +3,12 @@
   const { utils } = await import(src);
 
   function waitForElm(selector) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       if (document.querySelector(selector)) {
         return resolve(document.querySelector(selector));
       }
 
-      const observer = new MutationObserver(mutations => {
+      const observer = new MutationObserver((mutations) => {
         if (document.querySelector(selector)) {
           resolve(document.querySelector(selector));
           observer.disconnect();
@@ -17,24 +17,36 @@
 
       observer.observe(document.body, {
         childList: true,
-        subtree: true
+        subtree: true,
       });
     });
   }
 
-  waitForElm('.js-discussion-container').then(() => {
-    const allLinks = document.querySelectorAll('.note-text a');
+  waitForElm(".js-discussion-container").then(() => {
+    const allLinks = document.querySelectorAll(".note-text a");
     for (const anchorElement of allLinks) {
-      const hrefAtt = anchorElement.getAttribute('href');
-      if (hrefAtt.includes("https://www.drupal.org/project/")) {
-        const issueId = utils.getIssueIdFromUrl(hrefAtt);
-        // Make request to Drupal to get status.
-        if (!isNaN(issueId)) {
-          console.log(hrefAtt, issueId);
-        }
+      const hrefAtt = anchorElement.getAttribute("href");
+      const anchorText = anchorElement.innerHTML;
+      const issueId = utils.getIssueIdFromUrl(hrefAtt);
+      // Replace only if Drupal issue links.
+      if (
+        hrefAtt.includes("https://www.drupal.org/project/") &&
+        !isNaN(issueId) &&
+        hrefAtt === anchorText
+      ) {
+        fetch(`https://www.drupal.org/api-d7/node/` + issueId + `.json`)
+          .then((response) => response.json())
+          .then((data) => {
+            if (Object.keys(data).length !== 0) {
+              const issueStatus = utils.getStatusForId(data.field_issue_status);
+              anchorElement.text = "#" + issueId + ": " + data.title;
+              anchorElement.setAttribute("title", "Status: " + issueStatus);
+              anchorElement.classList.add(
+                "project-issue-status-" + data.field_issue_status
+              );
+            }
+          });
       }
     }
-
   });
-
 })();
